eda5ad11df3cbfa00cb8b35fb533f5098c82f449
Khang <khangvq.23itb@vku.udn.vn>
2024-10-29 22:02:11 +0700

    create database

---
+++ database/migrations/2024_10_29_101345_create_views_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Movie;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Movie::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('views');
+    }
+};
