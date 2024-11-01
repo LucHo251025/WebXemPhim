068fe0367c747db8a9301ad3f02ae1a4cbd614d4
Khang <khangvq.23itb@vku.udn.vn>
2024-11-01 10:33:39 +0700

    make ui#1

---
+++ resources/views/livewire/components/home-slider.blade.php
@@ -0,0 +1,52 @@
+<div class="swiper shrink-0 relative">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide max-h-[904px]" style="background: url({{ Vite::asset('resources/demo-images/img.png') }}) lightgray 50% / cover no-repeat; aspect-ratio: 16/9;">
+            <div name="homeSlider" class="homeSlider flex flex-col items-start gap-2.5 shrink-0 h-full">
+                <div class="shrink-0 self-stretch mt-auto mb-48 ml-32 flex-grow flex flex-col justify-end">
+                    <div name="content" class="flex flex-col items-start gap-4 w-1/2">
+                        <div class="flex flex-col items-start gap-[-24px] self-stretch">
+                            <span class="text-[22px] font-light leading-10">2022</span>
+                            <span class="font-extrabold text-5xl">The Batman</span>
+                            <span class="flex-col flex justify-center">2 hr 56 min • Action • Drama</span>
+                            <p class="w-[757px]">Batman is called to intervene when the mayor of Gotham City is murdered.
+                                Soon, his investigation leads him to uncover a web of corruption, linked to his own dark past.
+                            </p>
+                        </div>
+                        <div class="flex items-center gap-8">
+                            <button class="flex px-2.5 py-5.5 flex-col justify-center items-center gap-2.5 w-[151px] h-[44px]" style="border-radius: 0px 16px; background: var(--Primary, #7300FF);">Play Now</button>
+                            <button class="flex px-2.5 py-5.5 flex-col justify-center items-center gap-2.5 w-[151px] h-[44px]">Watch Trailer</button>
+                            <button class="bg-white text-black px-8 py-3 rounded-lg">Add To Wishlist</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="swiper-slide">
+            <div name="homeSlider" class="flex flex-col items-start gap-2.5 shrink-0 h-[904px]" style="background: url('https://placeholder.pics/svg/1920x904/70FF82') lightgray 50% / cover no-repeat;">
+                <div class="shrink-0 self-stretch mt-[465px] ml-32 ">
+                    <div name="content" class="flex flex-col items-start gap-4 w-1/2">
+                        <div class="flex flex-col items-start gap-[-24px] self-stretch">
+                            <span class="text-[22px] font-light leading-10">2022</span>
+                            <span class="font-extrabold text-5xl">The Batman</span>
+                            <span class="flex-col flex justify-center">2 hr 56 min • Action • Drama</span>
+                            <p class="w-[757px]">Batman is called to intervene when the mayor of Gotham City is murdered.
+                                Soon, his investigation leads him to uncover a web of corruption, linked to his own dark past.
+                            </p>
+                        </div>
+                        <div class="flex items-center gap-8">
+                            <button class="flex px-2.5 py-5.5 flex-col justify-center items-center gap-2.5 w-[151px] h-[44px]" style="border-radius: 0px 16px; background: var(--Primary, #7300FF);">Play Now</button>
+                            <button class="flex px-2.5 py-5.5 flex-col justify-center items-center gap-2.5 w-[151px] h-[44px] bg-white/5" style="border-radius: 0px 16px;">Watch Trailer</button>
+                            <button class="bg-white text-black px-8 py-3 rounded-lg">Add To Wishlist</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="swiper-slide">
+            <div class="flex flex-col items-start gap-2.5 shrink-0 h-[904px]" style="background: url('https://placeholder.pics/svg/1920x904/999') lightgray 50% / cover no-repeat;">123</div>
+        </div>
+    </div>
+    <div class="swiper-pagination mb-12"></div>
+</div>
