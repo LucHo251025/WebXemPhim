060be8309fd60e8cdd5a85a7f96c26e17e595d5d
MHuyen <huyenhtm.23itb@vku.udn.vn>
2024-11-08 12:43:51 +0700

    UI Upcoming movies

--- resources/views/livewire/upcoming-movies.blade.php
+++ resources/views/livewire/upcoming-movies.blade.php
@@ -5,7 +5,8 @@
     background-size: cover;
 ">
 
-        <div style="
+        <div
+            style="
                    height: 865px;
                    background: linear-gradient(353.54deg, #000000 9.64%, rgba(0, 0, 0, 0.731602) 37.09%, rgba(0, 0, 0, 0.468485) 45.71%, rgba(0, 0, 0, 0) 54.28%);">
 
@@ -17,8 +18,8 @@
                             align-items: end;
                             flex: 1;
                             ">
-                            <div style="flex: 1;">
-                                <div style="display: flex;
+                    <div style="flex: 1;">
+                        <div style="display: flex;
                                 flex-direction: column;
                                 align-items: flex-start;
                                 padding: 0px;
@@ -27,43 +28,45 @@
                                 font-weight: 700;
                                 line-height: 55.2px;
                                 text-align: left;">
-                        Get Ready for Action: Upcoming Blockbusters You Can't Miss!
+                            Get Ready for Action: Upcoming Blockbusters You Can't Miss!
 
-                            </div>
-                            <div style="display: flex;
+                        </div>
+                        <div style="display: flex;
                                         flex-direction: column;
                                         align-items: flex-start;
                                         font-family: Poppins,sans-serif;
-                                        font-size: 19px;
+                                        font-size: 18px;
                                         line-height: 22.8px;
                                         text-align: left;
                                         padding-top:10px
                                                         ">
-                                Prepare for an adrenaline-packed season with our upcoming blockbusters! From thrilling action
-                                sequences to heart-pounding adventures, these movies will keep you on the edge of your seat.
-                                Mark your calendars for an unforgettable cinematic experience!
-                            </div>
+                            Prepare for an adrenaline-packed season with our upcoming blockbusters! From thrilling
+                            action
+                            sequences to heart-pounding adventures, these movies will keep you on the edge of your seat.
+                            Mark your calendars for an unforgettable cinematic experience!
                         </div>
+                    </div>
 
-                            <div style="flex: 1;
+                    <div style="flex: 1;
                                        justify-items: center;
                                        display: flex;
                                        justify-content: center;
                               ">
-                    
-                 
-                <div style="    height: 119px; width: 100%;
+
+
+                        <div style="    height: 119px; width: 100%;
                                  justify-content: end;    display: flex;">
 
-                    <div style="width: 151px;
+                            <div style="width: 151px;
                                 height:  44px;
                                 padding: 10px 22px 10px 22px;
                                 gap: 10px;
                                 border-radius: 0px 16px 0px 16px;
                                border: 2px solid #FFFFFF;
                                background: var(--Transluse2, #3000774D);
+                                   margin-top: 70px;
                                ">
-                               <div style="
+                                <div style="
                                         font-family: Poppins,sans-serif;
                                         font-size: 15px;
                                         font-weight: 600;
@@ -73,26 +76,26 @@
                                             align-items: center;
                                             justify-content: center;
 ">
-                                            Movies
-                                            <div style="width: 17px;
+                                    Movies
+                                    <div style="width: 17px;
                                                         height: 11px;
                                                         margin-left: 5px;
                         
                                                     
 ">
 
-                                                     <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
+                                        <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
 
 
 
-                                            </div>
+                                    </div>
 
-                               </div>
+                                </div>
 
-                    </div>
+                            </div>
 
 
-                    <div style="width: 151px;
+                            <div style="width: 151px;
                     height:  44px;
                     padding: 10px 22px 10px 22px;
                     gap: 10px;
@@ -100,8 +103,9 @@
                    border: 2px solid #FFFFFF;
                    background: var(--Transluse2, #3000774D);
                        margin-left: 33px;
+                           margin-top: 70px;
                    ">
-                   <div style="
+                                <div style="
                             font-family: Poppins,sans-serif;
                             font-size: 15px;
                             font-weight: 600;
@@ -111,42 +115,42 @@
                                 align-items: center;
                                 justify-content: center;
 ">
-                               2024
-                                <div style="width: 17px;
+                                    2024
+                                    <div style="width: 17px;
                                             height: 11px;
                                             margin-left: 5px;
             
                                         
 ">
 
-                                         <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
+                                        <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
 
 
+
+                                    </div>
 
                                 </div>
 
-                   </div>
-
-        </div>
+                            </div>
 
 
-                </div>
-            </div>
-                   
+                        </div>
+                    </div>
 
-                    
+
+
 
                 </div>
 
-              
+
 
 
 
 
             </div>
 
-            
-   
+
+
 
 
         </div>
@@ -154,7 +158,7 @@
     <div style="margin: 50px 120px 0px 120px;background: #000000;">
 
         <div style="
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 29px;
 font-weight: 600;
 line-height: 34.8px;
@@ -184,25 +188,21 @@ text-align: left;
                         <div style="display: flex; margin: 10px 0px; width:45%">
                             <div style="width: 142px;
                     height: 142px;
-                    padding: 29px 37px 29px 37px;
-                    gap: 10px;
+                    padding: 29px 25px 29px 32px;   
                     border-radius: 71px;
                     border: 3px;
-                    opacity: 0px;
                     background: var(--Transluse2, #3000774D);
                     border: 3px solid #7300FF
 
 ">
                                 <div style="width: 68px;
 height: 77px;
-gap: 0px;
-opacity: 0px;
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 64px;
 font-weight: 700;
 line-height: 76.8px;
 text-align: left;
-
+color: var(--Primary, #7300FF); 
 ">14</div>
                             </div>
                             <div style="margin-left: 20px;">
@@ -216,14 +216,14 @@ border-radius: 29px;
     justify-content: center;
     margin-left: 20px;">
                                 <div style="
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 29px;
 font-weight: 600;
 line-height: 34.8px;
 text-align: left;
 ">Moana 2</div>
                                 <div style="
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 19px;
 font-weight: 400;
 line-height: 22.8px;
@@ -279,7 +279,7 @@ text-align: left;
 height: 77px;
 gap: 0px;
 opacity: 0px;
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 64px;
 font-weight: 700;
 line-height: 76.8px;
@@ -298,14 +298,14 @@ border-radius: 29px;
     justify-content: center;
     margin-left: 20px;">
                                 <div style="
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 29px;
 font-weight: 600;
 line-height: 34.8px;
 text-align: left;
 ">Moana 2</div>
                                 <div style="
-font-family: Poppins;
+font-family: Poppins,sans-serif;
 font-size: 19px;
 font-weight: 400;
 line-height: 22.8px;
@@ -361,7 +361,7 @@ text-align: left;
                                             height: 77px;
                                             gap: 0px;
                                             opacity: 0px;
-                                            font-family: Poppins;
+                                            font-family: Poppins,sans-serif;
                                             font-size: 64px;
                                             font-weight: 700;
                                             line-height: 76.8px;
@@ -379,13 +379,13 @@ text-align: left;
                                         flex-direction: column;
                                         justify-content: center;
                                         margin-left: 20px;">
-                                <div style="font-family: Poppins;
+                                <div style="font-family: Poppins,sans-serif;
                                             font-size: 29px;
                                             font-weight: 600;
                                             line-height: 34.8px;
                                             text-align: left;
                                                     ">Moana 2</div>
-                                <div style="font-family: Poppins;
+                                <div style="font-family: Poppins,sans-serif;
                                             font-size: 19px;
                                             font-weight: 400;
                                             line-height: 22.8px;
@@ -413,7 +413,7 @@ text-align: left;
                                border: 2px solid #FFFFFF;
                                background: var(--Transluse2, #3000774D);
                                ">
-                               <div style="
+                        <div style="
                                         font-family: Poppins,sans-serif;
                                         font-size: 15px;
                                         font-weight: 600;
@@ -422,21 +422,21 @@ text-align: left;
                                             display: flex;
                                             align-items: center;
 ">
-                                            Show more
-                                            <div style="width: 17px;
+                            Show more
+                            <div style="width: 17px;
                                                         height: 11px;
                                                         margin-left: 5px;
                         
                                                     
 ">
 
-                                                     <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
+                                <img src="{{ Vite::asset('resources/images/upcoming_image/Vector 3.png') }}">
 
 
 
-                                            </div>
+                            </div>
 
-                               </div>
+                        </div>
 
                     </div>
 
