4b1b1ad5cca41b98542863d3eeacfc177ba87296
MHuyen <huyenhtm.23itb@vku.udn.vn>
2024-11-09 16:48:21 +0700

    commit code use component

--- resources/views/livewire/upcoming-movies.blade.php
+++ resources/views/livewire/upcoming-movies.blade.php
@@ -164,90 +164,13 @@ text-align: left;
 ">
             Upcoming Movies
         </div>
-    <?php foreach ($groupedMovies as $year_month => $movies) {?>
-        <div style="margin: 50px 0px">
-            <div style="font-family: Poppins, SANS-SERIF;
-    font-size: 29px;
-    font-weight: 600;
-    line-height: 34.8px;
-    text-align: left;
-}
-">
-                {{ convertYearMonthToMonthName($year_month)}}
-                <hr style="    border: #A473FF 1px solid;
-    margin: 25px 0px;">
-
-                <div style="display: flex">
-                    <div style="display: flex;
-    flex:1;
-  flex-wrap: wrap;
-  flex-direction: row;">
-                        
-                    <?php foreach ($movies as $movie) {?>
-                        <div style="display: flex; margin: 10px 10px; width:45%; align-items: center;">
-                            <div style="width: 142px;
-                    height: 142px;
-                    border-radius: 71px;
-                    border: 3px;
-                    background: var(--Transluse2, #3000774D);
-                    border: 3px solid #7300FF;
-                        display: flex;
-    justify-content: center;
-    align-items: center;
-
-">
-                                <div style="width: 68px;
-height: 77px;
-font-family: Poppins,sans-serif;
-font-size: 64px;
-font-weight: 700;
-line-height: 76.8px;
-text-align: left;
-color: var(--Primary, #7300FF); 
-text-align: center;
-">{{ $movie->release_day_upcoming }}</div>
-                            </div>
-                            <div style="margin-left: 20px;">
-                                <img src="{{ Vite::asset($movie->links) }}" style="width: 134px;
-height: 125px;
-border-radius: 29px;
-">
-                            </div>
-                            <div style="display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-left: 20px;">
-                                <div style="
-font-family: Poppins,sans-serif;
-font-size: 29px;
-font-weight: 600;
-line-height: 34.8px;
-text-align: left;
-">{{ $movie->title_upcoming }}</div>
-                                <div style="
-font-family: Poppins,sans-serif;
-font-size: 19px;
-font-weight: 400;
-line-height: 22.8px;
-text-align: left;
-">Wordwide</div>
-                            </div>
-
-
-                        </div>
-                        <?php }?>
-                    </div>
-
-                </div>
-            </div>
-
-
-
-
-        </div>
+    <?php foreach ($groupedMovies as $year_month => $movies) {
+        $item=['year_month'=>$year_month, 'movies'=>$movies];
+        ?>
+       <x-upcoming-month :item="$item" />
         <?php }?>
 
-        <div style="margin: 30px 0px 0px 0px">            
+        <div style="margin: 30px 0px 0px 0px; padding-bottom:30px" >            
 
                 <div style="     width: 100%;
                                  justify-content: center;    display: flex;">
