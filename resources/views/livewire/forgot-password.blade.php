8d6168e340f376d64f316e3bc92dca0c02334377
Luc <lucht.23itb@vku.udn.vn>
2024-12-08 16:25:00 +0700

    reponsive forgot-password reset-password

--- resources/views/livewire/forgot-password.blade.php
+++ resources/views/livewire/forgot-password.blade.php
@@ -1,24 +1,22 @@
-<div class="flex w-full h-[100vh] p-[104px_90px] justify-center items-center" style="background: url({{ Vite::asset('resources/images/play_image/background.jpg') }}) lightgray 50% / cover no-repeat;">
-    <div class="flex w-[1740px] justify-between items-center flex-shrink-0">
-        <div class="flex w-[805px] flex-col items-center gap-12 flex-shrink-0">
-            <p class="flex h-36 flex-col justify-center self-stretch text-center font-normal font-poppins leading-[120%]"
-               style="font-size: 125px;font-weight: 700;color:#7300FF";>
+<div class="flex w-full h-screen p-4 sm:p-[20px] md:p-[104px_90px] justify-center items-center bg-cover bg-no-repeat" style="background: url({{ Vite::asset('resources/images/play_image/background.jpg') }}) lightgray 50% / cover no-repeat;">
+    <div class="flex w-full flex-col md:flex-row md:max-w-[1440px] justify-center items-center gap-4 md:gap-0">
+        <!-- Khối tiêu đề -->
+        <div class="flex w-full sm:w-auto flex-col items-center gap-4 sm:gap-6 md:gap-12 flex-shrink-0 mb-8 md:mb-0">
+            <p class="text-center font-poppins leading-[120%] text-2xl sm:text-3xl md:text-6xl font-bold" style="color:#7300FF;">
                 Cine Sphere
             </p>
-            <p class="self-stretch text-white text-center font-poppins leading-[120%] font-normal" style="font-size: 80px;font-weight: 700">
+            <p class="self-stretch text-white text-center font-poppins leading-[120%] text-sm sm:text-lg md:text-4xl font-bold">
                 The world’s Largest Movie Library
             </p>
         </div>
-        <div class="flex w-[777px] h-[872px] p-[88px_154px] justify-center items-center gap-3 flex-shrink-0" style="border-radius: 30px;border: 2px solid rgba(255, 255, 255, 0.10);background: var(--Glass, rgba(89, 77, 91, 0.20));
-    backdrop-filter: blur(24.5px);">
-            <div class="flex w-[408px] flex-col items-center gap-3 flex-shrink-0">
-                <div class="flex w-[356px] flex-col items-center gap-5">
+
+        <div class="flex w-full max-w-[320px] sm:max-w-[400px] p-4 md:max-w-[500px] md:h-auto justify-center items-center gap-3 flex-shrink-0" style="border-radius: 30px; border: 2px solid rgba(255, 255, 255, 0.10); background: var(--Glass, rgba(89, 77, 91, 0.20)); backdrop-filter: blur(24.5px);">
+            <div class="flex w-full sm:w-[320px] md:w-[408px] flex-col items-center gap-3 flex-shrink-0">
+                <div class="flex w-full flex-col items-center gap-3">
                     <div class="flex flex-col items-center gap-9 self-stretch">
-                        <div class="flex flex-col items-center self-stretch" style="gap: -20px">
-                            <div class="login-signup flex items-center gap-3">
-{{--                                <p class="text-white font-poppins leading-[120%]" style="font-size: 37px;font-weight: 700;">Login</p>--}}
-{{--                                <div class="w-[1px] h-[42px]" style="background: #FFF;"></div>--}}
-{{--                                <p class="font-poppins font-normal leading-[314.286%]" style="color:#A473FF;font-size: 35px;font-weight: 300;">Signup</p>--}}
+                        <div class="flex flex-col items-center self-stretch gap-4">
+                            <div class="login-signup flex items-center gap-3 justify-center">
+
                                 <p class="text-white font-poppins leading-[120%]" style="font-size: 37px;font-weight: 700;">Forgot Password</p>
 
                             </div>
@@ -27,8 +25,8 @@
                         <p class="self-stretch text-white text-center font-poppins leading-[120%]" style="font-size: 19px;font-weight: 600;">Or</p>
                     </div>
                 </div>
-                <form wire:submit.prevent="save">
-                    <div  class="flex flex-col items-start gap-8 self-stretch">
+                <form wire:submit.prevent="save" class="w-full">
+                    <div class="flex flex-col items-start gap-3 w-full">
                         @if(session('success'))
                             <div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
                                 <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -40,27 +38,29 @@
                                 </div>
                             </div>
                             @endsession
-                        <div class="flex flex-col items-start gap-3 self-stretch">
-                            <div class="flex flex-col items-start gap-8 self-stretch">
-                                <div class="email-login flex p-[22px_40px] flex-col items-start gap-3 self-stretch" style="border-radius: 8px;border: 1px solid var(--Electric-Violet-400, #A473FF);background: rgba(48, 0, 119, 0.10);">
-                                    <div class="flex justify-between items-center self-stretch">
-                                        <input type="email" id="email" wire:model="email"
-                                               class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none text-lg"
-                                               style="font-size: 20px; font-weight: 400;"
-                                               placeholder="Email Address"
-                                               required>
-                                        <img src="{{ Vite::asset('resources/images/play_image/iconamoon_profile-thin.svg') }}" alt="">
-                                    </div>
-                                </div>
 
-                            </div>
-                        </div>
-                        <div class="flex flex-col items-center gap-6 self-stretch">
-                            <button type="submit" class="flex h-[53px] p-[16px_50px] justify-center items-center gap-3 self-stretch" style="border-radius: 8px;background: #7300FF;">
-                                <p class="text-white font-poppins font-normal leading-[120%]" style="font-size: 15px;font-weight: 700;">Reset Password</p>
-                            </button>
+                        <!-- Email -->
+                        <div class="justify-between email-login flex p-3 sm:p-4 md:p-[15px_30px] items-center gap-3 w-full" style="border-radius: 8px;border: 1px solid #A473FF;background: rgba(48, 0, 119, 0.10);">
+                            <input type="email" id="email" wire:model="email"
+                                   class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none text-lg"
+                                   style="font-size: 20px; font-weight: 400;"
+                                   placeholder="Email Address"
+                                   required>
+                            <img src="{{ Vite::asset('resources/images/play_image/iconamoon_profile-thin.svg') }}" alt="">
                         </div>
 
+                    </div>
+
+                    <!-- Nút Sign Up -->
+                    <div class="flex flex-col items-center gap-6 mt-6">
+                        <button type="submit" class="w-full h-[48px] sm:h-[53px] flex justify-center items-center gap-3" style="border-radius: 8px;background: #7300FF;">
+                            <p class="text-white font-poppins font-bold text-sm sm:text-lg">Submit</p>
+                        </button>
+                        <div class="flex items-center gap-3">
+                            <img src="{{ Vite::asset('resources/images/play_image/ri_facebook-fill.svg') }}" alt="">
+                            <img src="{{ Vite::asset('resources/images/play_image/twitter.svg') }}" alt="">
+                            <img src="{{ Vite::asset('resources/images/play_image/mage_instagram-circle.svg') }}" alt="">
+                        </div>
                     </div>
                 </form>
             </div>
--- resources/views/livewire/reset-password.blade.php
+++ resources/views/livewire/reset-password.blade.php
@@ -1,91 +1,92 @@
-<div class="flex w-full h-[100vh] p-[104px_90px] justify-center items-center" style="background: url({{ Vite::asset('resources/images/play_image/background.jpg') }}) lightgray 50% / cover no-repeat;">
-    <div class="flex w-[1740px] justify-between items-center flex-shrink-0">
-        <div class="flex w-[805px] flex-col items-center gap-12 flex-shrink-0">
-            <p class="flex h-36 flex-col justify-center self-stretch text-center font-normal font-poppins leading-[120%]"
-               style="font-size: 125px;font-weight: 700;color:#7300FF";>
+<div class="flex w-full h-screen p-4 sm:p-[20px] md:p-[104px_90px] justify-center items-center bg-cover bg-no-repeat" style="background: url({{ Vite::asset('resources/images/play_image/background.jpg') }}) lightgray 50% / cover no-repeat;">
+    <div class="flex w-full flex-col md:flex-row md:max-w-[1440px] justify-center items-center gap-4 md:gap-0">
+        <!-- Khối tiêu đề -->
+        <div class="flex w-full sm:w-auto flex-col items-center gap-4 sm:gap-6 md:gap-12 flex-shrink-0 mb-8 md:mb-0">
+            <p class="text-center font-poppins leading-[120%] text-2xl sm:text-3xl md:text-6xl font-bold" style="color:#7300FF;">
                 Cine Sphere
             </p>
-            <p class="self-stretch text-white text-center font-poppins leading-[120%] font-normal" style="font-size: 80px;font-weight: 700">
+            <p class="self-stretch text-white text-center font-poppins leading-[120%] text-sm sm:text-lg md:text-4xl font-bold">
                 The world’s Largest Movie Library
             </p>
         </div>
 
-
-        <div class="flex w-[777px] h-[872px] p-[88px_154px] justify-center items-center gap-3 flex-shrink-0" style="border-radius: 30px;border: 2px solid rgba(255, 255, 255, 0.10);background: var(--Glass, rgba(89, 77, 91, 0.20));
-backdrop-filter: blur(24.5px);">
-            <div class="flex w-[408px] flex-col items-center gap-3 flex-shrink-0">
-                <div class="flex w-[356px] flex-col items-center gap-5">
-                    <div class="flex flex-col items-center gap-9 self-stretch">
-                        <div class="flex flex-col items-center self-stretch" style="gap: -20px">
-                            <div class="login-signup flex items-center gap-3">
-                                <p class="text-white font-poppins leading-[120%]" style="font-size: 37px;font-weight: 700;">Reset Password</p>
-                            </div>
-                            <p class="self-stretch text-center font-poppins font-normal leading-[120%]" style="font-weight: 400;font-size: 19px;color: rgba(255, 255, 255, 0.70);">Register Now to watch your favourite shows</p>
+        <div class="flex w-full max-w-[320px] sm:max-w-[400px] p-4 md:max-w-[500px] md:h-auto justify-center items-center gap-3 flex-shrink-0" style="border-radius: 30px; border: 2px solid rgba(255, 255, 255, 0.10); background: var(--Glass, rgba(89, 77, 91, 0.20)); backdrop-filter: blur(24.5px);">
+            <div class="flex w-full sm:w-[320px] md:w-[408px] flex-col items-center gap-3 flex-shrink-0">
+                <div class="flex w-full flex-col items-center gap-3">
+                    <div class="flex flex-col items-center self-stretch gap-4">
+                        <div class="login-signup flex items-center gap-3 justify-center">
+                            <p class="text-white font-poppins leading-[120%]" style="font-size: 37px;font-weight: 700;">Reset Password</p>
                         </div>
-                        <p class="self-stretch text-white text-center font-poppins leading-[120%]" style="font-size: 19px;font-weight: 600;">Or</p>
+                        <p class="self-stretch text-center font-poppins text-xs sm:text-sm md:text-base text-white opacity-70">
+                            Reset Password Now to watch your favourite shows
+                        </p>
                     </div>
                 </div>
-                <form wire:submit.prevent="save">
-                    <div class="flex flex-col items-start gap-8 self-stretch">
+                <p class="self-stretch text-white text-center font-poppins leading-[120%] font-semibold text-xs sm:text-base md:text-lg">
+                    Or
+                </p>
+                <form wire:submit.prevent="save" class="w-full">
+                    <div class="flex flex-col items-start gap-3 w-full">
                         @if(session('error'))
-                            <div class="w-full flex items-center p-3 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                                </svg>
-                                <span class="sr-only">Info</span>
-                                <div>
-                                    <span class="font-medium">{{session('error')}}</span>
-                                </div>
+                            <div class="w-full p-3 mb-4 text-sm text-red-800 bg-red-50 rounded-lg" role="alert">
+                                <span class="font-medium">{{session('error')}}</span>
                             </div>
-                            @endsession
-                        <div class="flex flex-col items-start gap-3 self-stretch">
-                            <div class="flex flex-col items-start gap-8 self-stretch">
-                                <div class="password-login flex p-[22px_40px] flex-col items-start gap-3 self-stretch" style="border-radius: 8px;border: 1px solid var(--Electric-Violet-400, #A473FF);background: rgba(48, 0, 119, 0.10);">
-                                    <div class="flex justify-between items-center self-stretch">
-                                        <input type="password" id="password" wire:model ='password'
-                                               class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none"
-                                               style="font-size: 20px; font-weight: 400;"
-                                               placeholder="Password"
-                                               required>
-                                        <img src="{{ Vite::asset('resources/images/play_image/tabler_lock-filled.svg') }}" alt="">
-                                    </div>
-                                </div>
-                                @error('password')
-                                <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                                    <span class="font-medium">{{$message}}</span>
-                                </div>
-                                @enderror
-                                <div class="password-login flex p-[22px_40px] flex-col items-start gap-3 self-stretch" style="border-radius: 8px;border: 1px solid var(--Electric-Violet-400, #A473FF);background: rgba(48, 0, 119, 0.10);">
-                                    <div class="flex justify-between items-center self-stretch">
-                                        <input type="hidden" wire:model="email" />
-                                        <input type="password" id="password_confirmation" wire:model="password_confirmation"
-                                               class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none"
-                                               style="font-size: 20px; font-weight: 400;"
-                                               placeholder="Confirm Password"
-                                               required>
-                                        <img src="{{ Vite::asset('resources/images/play_image/tabler_lock-filled.svg') }}" alt="">
-                                    </div>
-                                </div>
+                        @endif
 
-                            </div>
-                            <div class="flex items-center gap-3">
-                                <div class="flex w-[24px] h-[24px] p-[6px] items-center gap-3" style="border-radius: 12px;border: 1px solid #FFF;">
-                                    <img src="{{ Vite::asset('resources/images/play_image/Ellipse 12.svg') }}" alt="">
-                                </div>
-                                <p class="w-full h-[17px] text-white font-pooppins font-normal leading-[120%]" style="font-size: 12px;font-weight: 400;">I agree to the privacy policy & Terms and conditions</p>
-                            </div>
+                        <div class="justify-between email-login flex p-3 sm:p-4 md:p-[15px_30px] items-center gap-3 w-full" style="border-radius: 8px;border: 1px solid #A473FF;background: rgba(48, 0, 119, 0.10);">
+                            <input type="password" id="password" wire:model ='password'
+                                   class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none"
+                                   style="font-size: 20px; font-weight: 400;"
+                                   placeholder="Password"
+                                   required>
+                            <img src="{{ Vite::asset('resources/images/play_image/tabler_lock-filled.svg') }}" alt="">
                         </div>
-                        <div class="flex flex-col items-center gap-6 self-stretch">
-                            <button type="submit" class="flex h-[53px] p-[16px_50px] justify-center items-center gap-3 self-stretch" style="border-radius: 8px;background: #7300FF;">
-                                <p class="text-white font-poppins font-normal leading-[120%]" style="font-size: 15px;font-weight: 700;">Sign Up</p>
-                            </button>
-                            <div class="flex items-center gap-3">
-                                <img src="{{ Vite::asset('resources/images/play_image/ri_facebook-fill.svg') }}" alt="">
-                                <img src="{{ Vite::asset('resources/images/play_image/twitter.svg') }}" alt="">
-                                <img src="{{ Vite::asset('resources/images/play_image/mage_instagram-circle.svg') }}" alt="">
-                            </div>
+                        @error('password')
+                        <div class="p-1 mb-1 text-sm text-red-800 bg-red-50 rounded-lg" role="alert">
+                            <span class="font-medium">{{$message}}</span>
+                        </div>
+                        @enderror
+
+                        <div class="password-login justify-between flex p-3 sm:p-4 md:p-[15px_30px] items-center gap-3 w-full" style="border-radius: 8px;border: 1px solid #A473FF;background: rgba(48, 0, 119, 0.10);">
+                            <input type="hidden" wire:model="email" />
+                            <input type="password" id="password_confirmation" wire:model="password_confirmation"
+                                   class="text-white font-poppins leading-[120%] bg-transparent border-none focus:outline-none"
+                                   style="font-size: 20px; font-weight: 400;"
+                                   placeholder="Confirm Password"
+                                   required>
+                            <img src="{{ Vite::asset('resources/images/play_image/tabler_lock-filled.svg') }}" alt="">
                         </div>
 
+                        <!-- Remember Me và quên mật khẩu -->
+                        <div class="flex items-center gap-2 w-full">
+                            <!-- Radio button tùy chỉnh -->
+                            <div class="flex items-center relative">
+                                <!-- Radio button -->
+                                <input type="radio" id="rememberMe" name="remember" class="peer appearance-none w-6 h-6 rounded-full border border-white focus:outline-none" />
+
+                                <!-- Vòng tròn với chấm bên trong khi radio được chọn -->
+                                <div class="absolute inset-0 flex items-center justify-center">
+                                    <div class="w-3 h-3 bg-white rounded-full scale-0 peer-checked:scale-100 transition-transform duration-200"></div>
+                                </div>
+                            </div>
+
+                            <label for="rememberMe" class="text-white font-poppins text-xs sm:text-sm font-normal ml-2">I agree to the privacy policy & Terms and conditions</label>
+
+                            <!-- Forgot password -->
+                        </div>
+
+                    </div>
+
+                    <!-- Nút Sign Up -->
+                    <div class="flex flex-col items-center gap-6 mt-6">
+                        <button type="submit" class="w-full h-[48px] sm:h-[53px] flex justify-center items-center gap-3" style="border-radius: 8px;background: #7300FF;">
+                            <p class="text-white font-poppins font-bold text-sm sm:text-lg">Reset Password</p>
+                        </button>
+                        <div class="flex items-center gap-3">
+                            <img src="{{ Vite::asset('resources/images/play_image/ri_facebook-fill.svg') }}" alt="">
+                            <img src="{{ Vite::asset('resources/images/play_image/twitter.svg') }}" alt="">
+                            <img src="{{ Vite::asset('resources/images/play_image/mage_instagram-circle.svg') }}" alt="">
+                        </div>
                     </div>
                 </form>
             </div>
